005863aaa8dd4c60ddf1fa77d8946e8e63d15aa3
lnktoan <pjpo.frs1011@gmail.com>
2020-04-14 20:14:50 +0700

    Toan 14/4, part 2

--- app/src/main/java/com/example/food_order_application_2/Adapter/CustomAdapter.java
+++ app/src/main/java/com/example/food_order_application_2/Adapter/CustomAdapter.java
@@ -16,6 +16,7 @@ import com.example.food_order_application_2.R;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder>{
     ArrayList<food> data;
@@ -37,10 +38,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int min = 3;
+        int max = 5;
+        double random = new Random().nextDouble()*(max - min) +min;
 
         String link = data.get(position).getImg();
         holder.name.setText(data.get(position).getName());
         holder.price.setText(data.get(position).getPrice() + "$");
+        holder.rate.setText(String.format("%.1f", random));
         Picasso.get().load(link).resizeDimen(R.dimen.image_size, R.dimen.image_size).into(holder.imageView);
     }
 
@@ -50,7 +55,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView name, price,texTv;
+        TextView name, price, rate;
         LinearLayout linearLayout;
         ImageView imageView;
 
@@ -59,6 +64,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
             name = itemView.findViewById(R.id.nameProduct);
             price = itemView.findViewById(R.id.priceProduct);
             imageView = itemView.findViewById(R.id.imageViewFood);
+            rate = itemView.findViewById(R.id.tv_rate);
             //linearLayout = itemView.findViewById(R.id.clickLayout);
            // btnNumberCount = itemView.findViewById(R.id.number_counter);
         }
